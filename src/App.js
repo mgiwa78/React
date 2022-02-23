1e21024f26e735e957eb99b1999f73e06e0e25a5
Muhammad Giwa <mgiwa78@gmailcom>
2022-02-23 02:53:24 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import React, { Component } from 'react';
-import './App.css';
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/class-list/card-list.component";
 
-class App extends Component{
-  constructor(){
-    
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
 
-
-  return (){
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button>Change Text</button>
-      </header>
-    </div>
+  render() {
+    return (
+      <div className="App">
+        <CardList>
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+        </CardList>
+      </div>
+    );
   }
 }
 
